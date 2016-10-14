bebd0b6300c28887ae5c5098d4ae6a88d29b5196
EdGreen21 <edwardwilhelmgreen@gmail.com>
2016-10-14 17:58:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+threads=6
+
 # Extract fastq reads from bam file
+#check if new samtools implementation bam2fq is faster than bedtools implementation
 /download/bam2fastq/bam2fastq -o /workspace/${sampleID}#.fq -q /workspace/${inputBAM}.bam
 
 #step 2 repair .fq files and remove unrepaired
@@ -7,7 +10,7 @@
 rm /workspace/${sampleID}_1.fq.gz /workspace/${sampleID}_2.fq
 
 #step 3 run seq2HLA.py:
-python /download/seq2hla/seq2HLA.py -1 /workspace/${sampleID}_1.repair.fq -2 /workspace/${sampleID}_2.repair.fq -r "/workspace/${sampleID}"
+python /download/seq2hla/seq2HLA.py -1 /workspace/${sampleID}_1.repair.fq -2 /workspace/${sampleID}_2.repair.fq -r "/workspace/${sampleID}" -p ${threads}
 
 #step 4 remove files:
 rm /workspace/*.fq /workspace/*.sam /workspace/*.fq.gz 
