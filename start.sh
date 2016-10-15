50d42b58d1add09e63201aa499f23534c31b8bd2
EdGreen21 <edwardwilhelmgreen@gmail.com>
2016-10-15 20:57:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ threads=6
 /download/bam2fastq/bam2fastq -o /workspace/${sampleID}#.fq -q /workspace/${inputBAM}.bam
 
 #step 2 repair .fq files and remove unrepaired
-/download/bbmap/repair.sh in1=/workspace/${sampleID}_1.fq.gz in2=/workspace/${sampleID}_2.fq.gz out1=/workspace/${sampleID}_1.repair.fq out2=/workspace/${sampleID}_2.repair.fq
-rm /workspace/${sampleID}_1.fq.gz /workspace/${sampleID}_2.fq
+/download/bbmap/repair.sh in1=/workspace/${sampleID}_1.fq in2=/workspace/${sampleID}_2.fq out1=/workspace/${sampleID}_1.repair.fq out2=/workspace/${sampleID}_2.repair.fq
+rm /workspace/${sampleID}_1.fq /workspace/${sampleID}_2.fq
 
 #step 3 run seq2HLA.py:
 python /download/seq2hla/seq2HLA.py -1 /workspace/${sampleID}_1.repair.fq -2 /workspace/${sampleID}_2.repair.fq -r "/workspace/${sampleID}" -p ${threads}
