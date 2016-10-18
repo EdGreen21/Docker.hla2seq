176eb767c6bc947fb64a4d91037c3b84efaaa94a
EdGreen21 <edwardwilhelmgreen@gmail.com>
2016-10-18 14:55:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ samtools fastq -1 /workspace/${inputBAM}_1.fq -2 /workspace/${inputBAM}_2.fq /wo
 /download/bbmap/repair.sh in1=/workspace/${inputBAM}_1.fq in2=/workspace/${inputBAM}_2.fq out1=/workspace/${inputBAM}_1.repair.fq out2=/workspace/${inputBAM}_2.repair.fq
 
 #step 3: run seq2HLA with $threadsN
-python /download/seq2hla/seq2HLA.py -1 /workspace/${inputBAM}_1.repair.fq -2 /workspace/${inputBAM}_2.repair.fq -r "/workspace/${sampleID}" -p ${threadsN}
+python /download/seq2hla/seq2HLA.py -1 /workspace/${inputBAM}_1.repair.fq -2 /workspace/${inputBAM}_2.repair.fq -r "/workspace/${inputBAM}" -p ${threadsN}
 
 #step 4: cleanup extracted files to save disk space
 rm /workspace/*.fq
