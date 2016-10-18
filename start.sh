7dac2adea02e1c5a7c344a50d83d96da05a25207
EdGreen21 <edwardwilhelmgreen@gmail.com>
2016-10-18 09:43:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-samtools fastq -1 /workspace/${sampleID}_1.repair.fq -2 /workspace/${sampleID}_2.repair.fq /workspace/${inputBAM}.bam
+#step 1: extract fastq reads (samtools implementation seems fastest)
+samtools fastq -1 /workspace/${inputBAM}_1.fq -2 /workspace/${inputBAM}_2.fq /workspace/${inputBAM}.bam
 
-#/download/bam2fastq/bam2fastq -f -o /workspace/${sampleID}#.fq -q /workspace/${inputBAM}.bam
+#step 2: repair interleaving in *.fq files
+/download/bbmap/repair.sh in1=/workspace/${inputBAM}_1.fq in2=/workspace/${inputBAM}_2.fq out1=/workspace/${inputBAM}_1.repair.fq out2=/workspace/${inputBAM}_2.repair.fq
 
-#step 2 repair .fq files and remove unrepaired
-#'/download/bbmap/repair.sh in1=/workspace/${sampleID}_1.fq in2=/workspace/${sampleID}_2.fq out1=/workspace/${sampleID}_1.repair.fq out2=/workspace/${sampleID}_2.repair.fq
+#step 3: run seq2HLA with $threadsN
+python /download/seq2hla/seq2HLA.py -1 /workspace/${inputBAM}_1.repair.fq -2 /workspace/${inputBAM}_2.repair.fq -r "/workspace/${sampleID}" -p ${threadsN}
 
-python /download/seq2hla/seq2HLA.py -1 /workspace/${sampleID}_1.repair.fq -2 /workspace/${sampleID}_2.repair.fq -r "/workspace/${sampleID}" -p ${threads}
-
+#step 4: cleanup extracted files to save disk space
 rm /workspace/*.fq
