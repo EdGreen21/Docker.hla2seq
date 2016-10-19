ff74d92d0e0b362478a723dbe9deff7d3ca14259
EdGreen21 <edwardwilhelmgreen@gmail.com>
2016-10-20 00:13:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-#step 1: sort out chr6 reads
-samtools view -b /workspace/${inputBAM}.bam chr6 > /workspace/${inputBAM}.chr6.bam
+#step 1: extract fastq reads (samtools implementation seems fastest)
+samtools fastq -1 /workspace/${inputBAM}_1.fq -2 /workspace/${inputBAM}_2.fq /workspace/${inputBAM}.bam
 
-#step 2: extract fastq reads (samtools implementation seems fastest)
-samtools fastq -1 /workspace/${inputBAM}_1.fq -2 /workspace/${inputBAM}_2.fq /workspace/${inputBAM}.chr6.bam
-
-#step 3: repair interleaving in *.fq files
+#step 2: repair interleaving in *.fq files
 /download/bbmap/repair.sh in1=/workspace/${inputBAM}_1.fq in2=/workspace/${inputBAM}_2.fq out1=/workspace/${inputBAM}_1.repair.fq out2=/workspace/${inputBAM}_2.repair.fq
 rm /workspace/${inputBAM}_1.fq /workspace/${inputBAM}_2.fq
 
-#step 4: run seq2HLA with $threadsN
+#step 3: run seq2HLA with $threadsN
 python /download/seq2hla/seq2HLA.py -1 /workspace/${inputBAM}_1.repair.fq -2 /workspace/${inputBAM}_2.repair.fq -r "/workspace/${inputBAM}" -p ${threadsN}
 
-#step 5: cleanup extracted files to save disk space
-rm /workspace/*.fq /workspace/*.sam /workspace/*.chr6.bam
+#step 4: cleanup extracted files to save disk space
+rm /workspace/*.fq /workspace/*.sam
